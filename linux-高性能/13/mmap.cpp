caeda20fa57916def6b77c0b0c8b71680111ef0a
zhuwenboa <879658730@qq.com>
2020-04-08 15:56:29 +0800

    mmap练习

--- linux-高性能/13/mmap.cpp
+++ linux-高性能/13/mmap.cpp
@@ -8,129 +8,67 @@
 #include<sys/wait.h>
 #include<memory>
 #include<sys/types.h>
-static const char* shm_name = "/my_shm"; //共享内存文件名
-void pv(int sem_id, int op); //用信号量控制进程间同步
 
-class share_mem
+//mmap映射的文件不能为空，否则进行写操作时会产生总线错误。
+//对mmap返回的指针进行写操作，只能在文件原有大小基础上进行写入。
+
+class Mmap
 {
-public: 
-    share_mem();
-    ~share_mem()
-    {
-        shm_unlink(shm_name);
-    }
-    bool write_mem(char* str); //向共享内存中写入数据
-    bool read_mem(int len);           //从共享内存中读取数据
+public:  
+    Mmap(const char* file);
+    ~Mmap();
+    bool wirte(char*);
+    bool read(int);
+    int Fd();
 
-
-public:   
-    int pipefd[2];   //通知主/子进程有数据可读/可写
-private:  
-    char write_buf[1024];
-    char read_buf[1024];
-    int shmfd;      //共享内存描述符
-    std::shared_ptr<char*> sha_me; //指向共享内存的首地址
-    
-    //信号量
-    int sem_id;
-    union semun
-    {
-        int val;
-        struct semid_ds* buf;
-        unsigned short int* array;
-        struct seminfo* _buf;
-    }sem_un;     
+private:    
+    int fd;
+    char* share_mem;
 };
 
-share_mem::share_mem()
+Mmap::Mmap(const char* file)
 {
-    pipe(pipefd); //创建管道
-    bzero(write_buf, sizeof(write_buf));
-    bzero(read_buf, sizeof(read_buf));
-    //创建共享内存
-    shmfd = shm_open(shm_name, O_CREAT | O_RDWR | O_EXCL, 0666);
-    //改变共享内存大小
-    ftruncate(shmfd, 1024);
-
-    char* temp = (char*)mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
-    sha_me = std::make_shared<char*>(temp);
-    //创建信号量
-    sem_id = semget(IPC_PRIVATE, 1, 0666);
-    sem_un.val = 1; //将信号量设置为1
-
-    semctl(sem_id, 0, SETVAL, sem_un); //将信号量的semval值设置为1
-}
-
-
-//当op为-1时执行获取信号量操作，-1为释放信号量操作
-void pv(int sem_id, int op)
-{
-    struct sembuf sem_b;
-    sem_b.sem_num = 0; 
-    sem_b.sem_op = op;
-    sem_b.sem_flg = SEM_UNDO;
-    semop(sem_id, &sem_b, 1);
-}
-
-//向共享内存写入数据
-bool share_mem::write_mem(char* str)
-{
-    //像共享内存中写入数据，保证进程同步，op为-1 想要获取信号量
-    pv(sem_id, -1);
-    if(strlen(str) > 1023)
+    fd = open(file, O_CREAT | O_RDWR, 0666);
+    if(fd < 0)
     {
-        std::cout << "data is too long, the share_mem can't recv this data\n";
+        std::cout << "opne file failed\n";
+    }
+    char temp_mes[1024] = {0};
+    write(fd, temp_mes, 1024);
+    share_mem = (char*)mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+}
+
+Mmap::~Mmap()
+{
+    close(fd);
+    int ret = munmap((void*)share_mem, 1024);
+    if(ret != 0)
+    {
+        std::cout << "munmap failed\n";
+    }
+}
+
+bool Mmap::wirte(char* mes)
+{
+    strncpy(share_mem, mes, 1024);
+    return true;
+}
+
+bool Mmap::read(int size)
+{
+    if(size > 5 * 1024)
         return false;
-    }
-    else
-    {
-        for(int i = 0; i <= strlen(str); ++i)
-        {
-            (*sha_me)[i] = str[i];
-        }
-        std::cout << *sha_me << std::endl;
-    }
-    //将信号量加1，解除对信号量控制
-    pv(sem_id, 1);
-}
-
-//读取数据
-bool share_mem::read_mem(int len)
-{
-    pv(sem_id, -1);
-    for(int i = 0; i <= len; ++i)
-        read_buf[i] =(*sha_me)[i]; 
-    std::cout << "I read buf:  " <<  read_buf << std::endl;
-    pv(sem_id, 1);
+    char read_content[1024] = {0};
+    strncpy(read_content, share_mem, size);
+    std::cout << read_content << "\n";
+    return true;
 }
 
 int main()
 {
-    share_mem sha;
-    pid_t pid = fork();
-    if(pid < 0)
-    {
-        std::cout << "pid is false\n";
-    }
-    else if(pid == 0)
-    {
-        close(sha.pipefd[0]);
-        char* str = "hello word";
-        sha.write_mem(str);
-        int flag = strlen(str);
-        //通知主进程，共享内存中的数据写入，你可以读取
-        write(sha.pipefd[1], (char*)&flag, 4);
-    }
-    else
-    {
-        close(sha.pipefd[1]);
-        int flag;
-        //如果共享内存中没有数据，就将阻塞住
-        read(sha.pipefd[0], (char*)&flag, 4);
-        sha.read_mem(flag);
-    }
-    int status;
-    wait(&status);
-    return 0;
-
+    Mmap test("1.txt");
+    char* mes = "zhuwenbo";
+    test.wirte(mes);
+    //test.wirte("zhuwenbo\0");
+    test.read(1000);
 }
