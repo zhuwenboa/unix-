531a13eeabd4e2de1e6d2544a1cdb7adda9dd7ee
zhuwenboa <879658730@qq.com>
2021-02-15 21:13:06 +0800

    EPOLLONESHOT

--- linux-高性能/9/EPOLLONESHOT.cpp
+++ linux-高性能/9/EPOLLONESHOT.cpp
@@ -1,4 +1,5 @@
 #include"head.hpp"
+#include<vector>
 
 #define MAX_EVENT_NUMBER 1024
 #define BUFFER_SIZE 1024
@@ -34,45 +35,104 @@ void reset_oneshot(int epollfd, int fd)
 {
     epoll_event event;
     event.data.fd = fd;
-    event.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
-    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    event.events = EPOLLIN | EPOLLONESHOT | EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
 }
 
-/*工作线程*/
-void* worker(void *arg)
+void movfd(int epollfd, int fd)
 {
-    int sockfd = ( (fds*)arg )->sockfd;
-    int epollfd = ( (fds*)arg )->epollfd;
-    std::cout << "start new thread to receive data on fd :" << sockfd << "\n";
-    char buf[BUFFER_SIZE] = {0};
-    /*循环读取sockfd上的数据，直到遇到EAGAIN错误*/
-    while(1)
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
+    close(fd);
+}
+
+int main()
+{
+    const char* ip = "127.0.0.1";
+    int port = 8888;
+    struct sockaddr_in ser_addr;
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_port = htons(port);
+    ser_addr.sin_addr.s_addr = INADDR_ANY;
+
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    setnonblocking(listenfd);
+    int ret = bind(listenfd, (struct sockaddr*)&ser_addr, sizeof(ser_addr));
+    ret = listen(listenfd, 5);
+
+    int epollfd = epoll_create(5);
+    std::vector<epoll_event> ser_event;
+    ser_event.reserve(100);
+
+    addfd(epollfd, listenfd, true);
+
+    while(true)
     {
-        int ret = recv(sockfd, buf, BUFFER_SIZE-1, 0);
-        if(ret == 0)
+        int numbers = epoll_wait(epollfd, &*ser_event.begin(), 100, -1);
+        for(int i = 0; i < numbers; ++i)
         {
-            close(sockfd);
-            std::cout << "foreiner closed the connection\n";
-            break;
-        }
-        else if(ret < 0)
-        {
-            if(errno == EAGAIN)
+            int fd = ser_event[i].data.fd;
+            if(fd == listenfd && ser_event[i].events & EPOLLIN)
             {
-                reset_oneshot(epollfd, sockfd);
-                std::cout << "read later\n";
-                break;
+                struct sockaddr_in cli_addr;
+                int len = sizeof(cli_addr);
+                while(true)
+                {
+                    int connfd = accept(listenfd, (struct sockaddr*)&cli_addr, (socklen_t*)&len);
+                    if(connfd < 0)
+                    {
+                        if(errno == EAGAIN || errno == EWOULDBLOCK)
+                        {
+                            std::cout << "没有更多的连接可以建立\n";
+                            break;
+                        }
+                        else 
+                        {
+                            exit(-1);
+                        }
+                    }
+                    setnonblocking(connfd);
+                    addfd(epollfd, connfd, true);
+                }
+            }
+            else if(ser_event[i].events & EPOLLIN)
+            {
+                std::vector<char> buf;
+                buf.reserve(1024);
+                int able_recv = 1024;
+                int ret;
+                    while(1)
+                    {
+                        ret = recv(fd, &*buf.begin(), able_recv, 0);
+                        if(ret == 0)
+                        {
+                            movfd(epollfd, fd);
+                            std::cout << "foreiner closed the connection\n";
+                            break;
+                        }
+                        else if(ret < 0)
+                        {
+                            if(errno == EAGAIN || errno == EWOULDBLOCK)
+                            {
+                                std::cout << "即将休眠\n";
+                                sleep(5);
+                                reset_oneshot(epollfd, fd);
+                                std::cout << "read later\n";
+                                break;
+                            }
+                            else
+                            {
+                                std::cout << "recv failed\n";
+                                /*模拟数据处理过程*/
+                                break;
+                            }           
+                        }
+                         able_recv -= ret;
+                    }
+                    std::cout << "read buf = " << buf[0] << "\n";
+                    std::cout << "end thread receving data on fd: " << fd << "\n";
             }
         }
-        else
-        {
-            std::cout << "get content: " << buf << std::endl;
-            /*模拟数据处理过程*/
-            sleep(5);
-        }
-        
     }
-    std::cout << "end thread receving data on fd: " << sockfd << "\n";
-    
 }
 
